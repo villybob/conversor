b0b9af06a14709793883101239b2679718bb7f19
Villullas <cesarvillullas@gmail.com>
2018-12-13 12:41:23 +0100

    Funcionalidad del converter terminada
    Está toda la funcionalidad del conversor terminada. De mometo llama a la API propia de Bit2Me hasta que se complemente la nueva API.

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@
     <div class="content_body_wrap">
       <div class="content_body">
         <Title/>
-        <div id="converter_template_wrap" class="converter_template_wrap">
-          <converter/>
-          <converter v-for="converted in converters" :key="converted"/>
+        <div id="converter_template_wrap" class="converter_template_wrap" v-if="api.length">
+          <converter :api="api"/>
+          <converter :api="api" v-for="converted in converters" :key="converted"/>
           <div id="plus_wrap" class="plus_wrap">
             <div class="plus">
                 <img @click="addConverter()" class="plus_img" src="../dist/img/plus.png">
@@ -23,6 +23,7 @@ import Header from './components/Header.vue'
 import Title from './components/Title.vue'
 import converter from './components/converter.vue'
 import VueAutonumeric from 'vue-autonumeric'
+import axios from 'axios'
 
 require('@/../dist/css/styles.css');
 
@@ -37,7 +38,24 @@ export default {
   data(){
     return {
       converters: [],
+      urlApi: 'https://api.bit2me.com/v1/ticker2',
+      refreshTimeInterval: 5000,
+      api:{}
     }
+  },
+  created (){
+    axios
+      .get(this.urlApi)
+      .then(response => {
+          this.api = response.data.data
+      });
+
+    setInterval(() => { axios
+      .get(this.urlApi)
+      .then(response =>{
+          this.api = response.data.data;
+      });
+    }, this.refreshTimeInterval);
   },
   methods: {
     addConverter(){
--- src/components/converter.vue
+++ src/components/converter.vue
@@ -1,6 +1,5 @@
 <template>
     <div id="template_wrap">
-        <p>{{coinSelected}}</p>
         <div class="full_converter_wrap">
             <div class="converter">
                 <div class="converter_wrap">
@@ -11,17 +10,18 @@
                                 digitGroupSeparator: '',
                                 minimumValue: '0',
                             }"
-                            v-model="n1">
+                            v-model="valueInput1">
                         </vue-autonumeric> -->
-                        <input ref="amount_left" class="amount" type="number" v-model="n1" @focus="operation(); oper=true">
+                        <input ref="amount_left" class="amount" type="number" v-model="valueInput1" @focus="isInput1Focus=true">
                     </div>
                     <div class="coin_wrap">
-                        <div v-if="dropdown">
+                        <div v-if="openedDropdown1">
                             <vue-single-select 
                                 option-label="name"
-                                :options="api"
-                                v-model="coinSelected"
-                                >
+                                :options="apiData"
+                                v-model="coinSelected1"
+                                placeholder="insert coin"
+                                    >
                                     <template slot="option" slot-scope="{option}">
                                         <div class="select_coin_wrap">
                                             <div class="coin_logo">
@@ -42,19 +42,19 @@
                         </div>
                         <br>
                         <div class="coin_wrap" @click="handleSelectCoin()">
-                            <div class="coin_logo" v-if="!dropdown">
-                                <img class="logo" :src="logo">
+                            <div class="coin_logo" v-if="!openedDropdown1">
+                                <img class="logo" :src="coinSelected1.icon">
                             </div>
-                            <div class="coin_symbol" v-if="!dropdown">
-                                <span class="symbol">{{ symbol1 }}</span>
+                            <div class="coin_symbol" v-if="!openedDropdown1">
+                                <span class="symbol">{{ coinSelected1.symbol }}</span>
                             </div>
-                            <div class="coin_separator" v-if="!dropdown">
+                            <div class="coin_separator" v-if="!openedDropdown1">
                                 <span class="separator">-</span>
                             </div>
-                            <div class="coin_name" v-if="!dropdown">
-                                <span class="name">{{ name1 }}</span>
+                            <div class="coin_name" v-if="!openedDropdown1">
+                                <span class="name">{{ coinSelected1.name }}</span>
                             </div>
-                            <div class="selector_arrow_wrap" v-if="!dropdown">
+                            <div class="selector_arrow_wrap" v-if="!openedDropdown1">
                                 <div class="selector_arrow">
                                     <img class="arrow_down" src="../../dist/img/arrow_down.svg">
                                 </div>
@@ -73,40 +73,65 @@
                                 digitGroupSeparator: '',
                                 minimumValue: '0',
                             }"
-                            v-model="n2" value=1>
+                            v-model="valueInput2" value=1>
                         </vue-autonumeric> -->
-                        <input class="amount" type="number" v-model="n2" @focus="operation(); oper=false">
+                        <input class="amount" type="number" v-model="valueInput2" @focus="isInput1Focus=false">
                     </div>
                     <div class="coin_wrap">
-                        <div class="coin_logo">
-                            <img class="logo" src= "../../dist/img/euro.png">
+                        <div v-if="openedDropdown2">
+                            <vue-single-select 
+                                option-label="name"
+                                :options="apiData"
+                                v-model="coinSelected2"
+                                placeholder="insert coin"
+                                >
+                                    <template slot="option" slot-scope="{option}">
+                                        <div class="select_coin_wrap">
+                                            <div class="coin_logo">
+                                                <img class="select_logo" :src="option.icon">
+                                            </div>
+                                            <div class="coin_symbol">
+                                                <span class="select_symbol">{{ option.symbol }}</span>
+                                            </div>
+                                            <div class="coin_separator">
+                                                <span class="separator">-</span>
+                                            </div>
+                                            <div class="coin_name">
+                                                <span class="name">{{ option.name }}</span>
+                                            </div>
+                                        </div>
+                                    </template>
+                            </vue-single-select>
                         </div>
-                        <div class="coin_symbol">
-                            <span class="symbol">{{ symbol2 }}</span>
-                        </div>
-                        <div class="coin_separator">
-                            <span class="separator">-</span>
-                        </div>
-                        <div class="coin_name">
-                            <span class="name">{{ name2 }}</span>
-                        </div>
-                        <div class="selector_arrow_wrap">
-                            <div class="selector_arrow">
-                                <img  class="arrow_down" src="../../dist/img/arrow_down.svg">
+                        <div class="coin_wrap" @click="handleSelectCoin2()">
+                            <div class="coin_logo" v-if="!openedDropdown2">
+                                <img class="logo" :src="coinSelected2.icon">
+                            </div>
+                            <div class="coin_symbol" v-if="!openedDropdown2">
+                                <span class="symbol">{{ coinSelected2.symbol }}</span>
+                            </div>
+                            <div class="coin_separator" v-if="!openedDropdown2">
+                                <span class="separator">-</span>
+                            </div>
+                            <div class="coin_name" v-if="!openedDropdown2">
+                                <span class="name">{{ coinSelected2.name }}</span>
+                            </div>
+                            <div class="selector_arrow_wrap" v-if="!openedDropdown2">
+                                <div class="selector_arrow">
+                                    <img  class="arrow_down" src="../../dist/img/arrow_down.svg">
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>    
             </div>      
-        </div> {{coin}}
+        </div>
     </div>       
 </template>
 
 <script>
 import VueAutonumeric from 'vue-autonumeric';
 import VueSingleSelect from "vue-single-select";
-const axios = require('axios');
-const urlApi = 'https://api.bit2me.com/v1/ticker2';
 
 export default {
     name: 'converter',
@@ -116,90 +141,96 @@ export default {
         VueSingleSelect,
     },
 
-    props: {
-
-    },
+    props: [
+        'api'
+    ],
 
     data(){
         return {
-            dropdown: false,
-            oper: true,
-            n1: 1,
-            symbol1: 'BTC',
-            symbol2: 'EUR',
-            name1: 'Bitcoin',
-            name2: 'Euro',
-            logo: '',
-            //coinValue: 3000,
-            n2: 3000,
-            coin:[],
-            api: {},
-            apiCoin:{},
-            coinSelected: {}
+            openedDropdown1: false,
+            openedDropdown2: false,
+            isInput1Focus: true,
+            isInitial: true,
+            valueInput1: 1,
+            valueInput2: 1,
+            coinSelected1: {
+                name: '',
+                symbol: '',
+                icon: '',
+                buy: 1,
+            },
+            coinSelected2: {
+                name: 'Euro',
+                symbol: 'EUR',
+                icon: '../../dist/img/euro.png',
+                buy: 1,
+            },
+            refreshTimeInterval: 5000,
+            apiData: [],
         }
     },
 
     watch: {
-        coinSelected(){
-            if(this.coinSelected.hasOwnProperty('name')){
-                this.dropdown=!this.dropdown;
-                this.logo=this.coinSelected.icon;
-                this.symbol1=this.coinSelected.symbol;
-                this.name1=this.coinSelected.name;
-                this.coin=this.coinSelected.buy;
-
+        coinSelected1(){ 
+            if(this.coinSelected1.hasOwnProperty('name')){
+                this.openedDropdown1=false;
+                this.initial=false;
+                this.operation();
             }
-        }
+        },
+        coinSelected2(){ 
+            if(this.coinSelected2.hasOwnProperty('name')){
+                this.openedDropdown2=!this.openedDropdown2;
+                this.operation();
+            }
+        },
+        valueInput1(){
+            if(this.isInput1Focus){
+                this.valueInput2 = this.valueInput1*this.coinSelected1.buy/this.coinSelected2.buy; 
+            }
+       },
+        valueInput2(){
+            if(!this.isInput1Focus){
+                this.valueInput1 = this.valueInput2*this.coinSelected2.buy/this.coinSelected1.buy;
+            }
+        },
+        api(newVal, oldVal){
+            this.apiData = newVal;
+        
+            this.api.map((coin)=>{
+                if(this.coinSelected1.symbol === coin.symbol){
+                    this.coinSelected1.buy = coin.buy;
+                    }
+                if(this.coinSelected2.symbol === coin.symbol){
+                    this.coinSelected2.buy = coin.buy;
+                }
+            });
+            this.operation();
+        } 
     },
 
     mounted () {
         this.$refs.amount_left.focus();
-        setInterval(() => {
-            if(this.oper){
-                this.n2 = this.n1*this.coin;     
-            }else{
-                this.n1 = this.n2/this.coin;
-            }
-        }, 5000);
-    },
-
-    created (){
-        axios
-            .get(urlApi)
-            .then(response => (
-                this.coin = response.data.data[0].buy,
-                this.n2 = response.data.data[0].buy,
-                this.logo = response.data.data[0].icon,
-                this.api = response.data.data
-            ))
-        setInterval(() => { axios
-            .get(urlApi)
-            .then(response => (this.coin = response.data.data[0].buy, this.api = response.data.data))
-        }, 5000)
-        
+        this.coinSelected1 = this.api[0];
+        this.valueInput2 = this.api[0].buy;
     },
 
     methods: {
-        operation: function(){     
-            setInterval(() => {
-                if(this.oper){
-                    this.n2 = this.n1*this.coin;     
-                }else{
-                    this.n1 = this.n2/this.coin;
-                }
-            }, 1);
+        operation(){
+            if(this.isInput1Focus){
+                this.valueInput2 = this.valueInput1*this.coinSelected1.buy/this.coinSelected2.buy;     
+            }else{
+                this.valueInput1 = this.valueInput2*this.coinSelected2.buy/this.coinSelected1.buy;
+            }
         },
         handleSelectCoin(){
-            this.dropdown = true;
-            this.coinSelected = {};
+            this.openedDropdown1 = true;
+            this.coinSelected1 = {};
+        },
+        handleSelectCoin2(){
+            this.openedDropdown2 = true;
+            this.coinSelected2 = {};
         }
-    // directives: {
-    //     focus: {
-    //         // directive definition
-    //         inserted: function (el) {
-    //         el.focus()
-    //         }
-    //     }
     }
 }
 
